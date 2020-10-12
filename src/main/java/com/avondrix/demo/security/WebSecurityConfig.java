9b56959877f3bb919d49ed17d29f063637d61be9
avondrix <duo.1080@gmail.com>
2020-10-12 23:22:56 +0800

    setup role

--- src/main/java/com/avondrix/demo/security/WebSecurityConfig.java
+++ src/main/java/com/avondrix/demo/security/WebSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.avondrix.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -15,6 +30,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails avonUser = User.builder()
+                .username("avon")
+                .password(passwordEncoder.encode("password"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails DuoUser = User.builder()
+                .username("DUO)")
+                .password(passwordEncoder.encode("admin password"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                avonUser,
+                DuoUser
+        );
     }
 }
